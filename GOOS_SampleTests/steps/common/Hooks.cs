e43252c74e2e577a8a1c5995a99165c41859f025
Chen Yu Pao <windperson@gmail.com>
2017-07-01 15:49:10 +0800

    DI in test project.

--- GOOS_SampleTests/steps/common/Hooks.cs
+++ GOOS_SampleTests/steps/common/Hooks.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FluentAutomation;
+using GOOS_Sample.Controllers;
+using GOOS_Sample.Models;
 using GOOS_SampleTests.DataModelsForIntegrationTest;
+using Microsoft.Practices.Unity;
 using TechTalk.SpecFlow;
 
 namespace GOOS_SampleTests.steps.common
@@ -12,6 +15,15 @@ namespace GOOS_SampleTests.steps.common
     public sealed class Hooks
     {
         // For additional details on SpecFlow hooks see http://go.specflow.org/doc-hooks
+
+        public static UnityContainer UnityContainer { get; set; }
+
+        [BeforeTestRun]
+        public static void RegisterDIContainer()
+        {
+            UnityContainer = new UnityContainer();
+            UnityContainer.RegisterType<IBudgetService, BudgetService>();
+        }
 
         [BeforeScenario()]
         public void BeforeScenarioCleanTable()
