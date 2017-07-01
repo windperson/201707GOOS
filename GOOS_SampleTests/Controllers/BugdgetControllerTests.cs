cddcaaeda2aa059b6522e82f25e791baf66121dc
Chen Yu Pao <windperson@gmail.com>
2017-07-01 18:16:18 +0800

    add event test (green light)

--- GOOS_SampleTests/Controllers/BugdgetControllerTests.cs
+++ GOOS_SampleTests/Controllers/BugdgetControllerTests.cs
@@ -36,14 +36,65 @@ namespace GOOS_SampleTests.Controllers
         [TestMethod()]
         public void CreateTest_when_exist_record_should_update_budget()
         {
+
             this._budgetService = new BudgetService(_budgetRepositoryStub);
+
             var budgetFromDb = new Budget { Amount = 999, YearMonth = "2017-02" };
+
             _budgetRepositoryStub.Read(Arg.Any<Func<Budget, bool>>())
                 .ReturnsForAnyArgs(budgetFromDb);
+
             var model = new BudgetAddViewModel { Amount = 2000, Month = "2017-02" };
+
+            var wasUpdated = false;
+            this._budgetService.Updated += (sender, args) => { wasUpdated = true; };
+
             this._budgetService.Create(model);
+
             _budgetRepositoryStub.Received()
                 .Save(Arg.Is<Budget>(x => x == budgetFromDb && x.Amount == 2000));
+
+            Assert.IsTrue(wasUpdated);
+        }
+
+        [TestMethod()]
+        public void CreateTest_should_invoke_repository_one_time()
+        {
+            this._budgetService = new BudgetService(_budgetRepositoryStub);
+
+            var model = new BudgetAddViewModel { Amount = 2000, Month = "2017-02" };
+
+            var wasCreate = false;
+            this._budgetService.Created += (sender, args) => { wasCreate = true; };
+            this._budgetService.Create(model);
+
+            _budgetRepositoryStub.Received()
+                .Save(Arg.Is<Budget>(x => x.Amount == 2000 && x.YearMonth == "2017-02"));
+
+            Assert.IsTrue(wasCreate);
+        }
+
+        [TestMethod()]
+        public void CreateTest_when_exist_record_should_update_update_budget()
+        {
+            this._budgetService = new BudgetService(_budgetRepositoryStub);
+
+            var budgetFromDb = new Budget { Amount = 999, YearMonth = "2017-02" };
+
+            _budgetRepositoryStub.Read(Arg.Any<Func<Budget, bool>>())
+                .ReturnsForAnyArgs(budgetFromDb);
+
+            var model = new BudgetAddViewModel { Amount = 2000, Month = "2017-02" };
+
+            var wasUpdated = false;
+            this._budgetService.Updated += (sender, args) => { wasUpdated = true; };
+
+            this._budgetService.Create(model);
+
+            _budgetRepositoryStub.Received()
+                .Save(Arg.Is<Budget>(x => x == budgetFromDb && x.Amount == 2000));
+
+            Assert.IsTrue(wasUpdated);
         }
 
     }
